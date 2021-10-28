2081138fa58bc29f38f11f762a1792603cf45949
Artemiy Schukin <shuckinart@yandex.ru>
2021-10-28 18:58:56 +0300

    Try to add benchmarks

---
+++ benchmark/benchmark.ts
@@ -0,0 +1,41 @@
+import Go from "./wasm_exec.ts";
+import { swagify as swagifyTS } from "https://deno.land/x/swagify@v1.1.2/mod.ts";
+
+declare global {
+  var swagify: typeof swagifyTS;
+}
+
+const go = new Go();
+
+const wasmCode = await Deno.readFile("../dist/main.wasm");
+const wasmModule = new WebAssembly.Module(wasmCode);
+const instance = await WebAssembly.instantiate(wasmModule, go.importObject);
+
+go.run(instance);
+
+import {
+  bench,
+  runBenchmarks,
+} from "https://deno.land/std@0.113.0/testing/bench.ts";
+
+bench({
+  name: "WASM Swagify",
+  runs: 1e5,
+  func(b): void {
+    b.start();
+    swagify("Artemiy Shuckin", {});
+    b.stop();
+  },
+});
+
+bench({
+  name: "TS Swagify",
+  runs: 1e5,
+  func(b): void {
+    b.start();
+    swagifyTS("Artemiy Shuckin", {});
+    b.stop();
+  },
+});
+
+runBenchmarks();
